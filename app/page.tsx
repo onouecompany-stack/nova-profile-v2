2016b008a753694284e3123377ef068a438b30ce
onouecompany-stack <onoue.company@gmail.com>
2026-08-06 20:59:47 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>NOVA Profile App</h1>
+      <p>プロフィールページへようこそ！</p>
+    </main>
+  );
+}
