268f4c40ab6416bb7cc8216e2517675d302d9bfd
onouecompany-stack <onoue.company@gmail.com>
2026-08-06 21:52:13 +0900

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{ padding: "2rem" }}>
-      <h1>Nova Profile App</h1>
-      <p>API is ready at /api/profile</p>
-    </main>
-  );
-}
